eb5eb2de1b0d9270d2d4980af0392da1832798e5
lingalaabhi34 <122089325+lingalaabhi34@users.noreply.github.com>
2024-02-08 17:49:36 +0000

    FINAL

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,7 @@ import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-        const [data,setdata] = useState("");
+    const [data,setdata] = useState("");
     const [output,setoutput]=useState([]);
     function inputdata(e){
         setdata(e.target.value)
@@ -21,8 +18,11 @@ setdata("");
     function deltetodo(index){
        setoutput(output.filter((item,i)=>i !== index ))
     }
-    return(
-        <div>
+  return (
+    <div>
+        {/* Do not remove the main div */}
+ 
+   
        <form onSubmit={Adddata}>
        <input type="text" placeholder="enter data" onChange={inputdata} value={data}></input>
 <button type="submit">Add Todo</button>
@@ -38,8 +38,7 @@ setdata("");
         ))
        }
         
-        </div>
-    )
+    
     </div>
   )
 }
